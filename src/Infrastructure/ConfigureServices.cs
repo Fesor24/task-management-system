12ea0a55a27387061fca4f0fc090cc10ff5a5932
Wale <onafesowale@gmail.com>
2023-09-09 20:46:46 +0100

    register services

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Context;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Infrastructure;
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"),
+                migrations => migrations.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
