87ce88b8ac1f95efa2e692198e1b9d41d9382aaf
Wale <onafesowale@gmail.com>
2023-09-09 20:43:36 +0100

    Auditable entity

---
+++ src/Domain/Entities/AuditableEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Entities;
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
