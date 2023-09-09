5637091d2e9a493eac468846400b5cad2869f0e0
Wale <onafesowale@gmail.com>
2023-09-09 20:46:13 +0100

    Specification Evaluator

---
+++ src/Infrastructure/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Specifications;
+public static class SpecificationEvaluator
+{
+    public static IQueryable<TEntity> GetQuery<TEntity>(IQueryable<TEntity> query, ISpecification<TEntity> spec) 
+        where TEntity : class
+    {
+        IQueryable<TEntity> queryToReturn = query;
+
+        if(spec.Criteria is not null)
+        {
+            queryToReturn = queryToReturn.Where(spec.Criteria);
+        }
+
+        if(spec.OrderBy is not null)
+        {
+            queryToReturn = queryToReturn.OrderBy(spec.OrderBy);
+        }
+
+        if(spec.OrderByDesc is not null) 
+        {
+            queryToReturn = queryToReturn.OrderByDescending(spec.OrderByDesc);
+        }
+
+        queryToReturn = spec.Includes.Aggregate(queryToReturn, (current, include) => current.Include(include));
+
+        return queryToReturn;
+    }
+}
