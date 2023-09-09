da44e60633c623a14c6a72c6311fa1377fc79d1b
Wale <onafesowale@gmail.com>
2023-09-09 20:45:42 +0100

    TaskConfiguration

---
+++ src/Infrastructure/EntityConfigurations/Common/TaskConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskEntity = Domain.Entities.Common.Task;
+
+namespace Infrastructure.EntityConfigurations.Common;
+public class TaskConfiguration : IEntityTypeConfiguration<TaskEntity>
+{
+    public void Configure(EntityTypeBuilder<TaskEntity> builder)
+    {
+        builder.ToTable(nameof(Task), "com");
+        builder.Property(x => x.Title).IsRequired().HasColumnName("Title");
+        builder.Property(x => x.Status).HasColumnName("Status");
+        builder.Property(x => x.Priority).HasColumnName("Priority");
+        builder.Property(x => x.DueDate).HasColumnName("DueDate");
+        builder.Property(x => x.Description).IsRequired(false).HasColumnName("Description");
+    }
+}
