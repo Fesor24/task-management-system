2c4fceac75fb882781ee2ea74ac13f47b6f45733
Wale <onafesowale@gmail.com>
2023-09-09 20:43:59 +0100

    ISpecification

---
+++ src/Domain/Abstractions/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Abstractions;
+public interface ISpecification<T> where T : class
+{
+    Expression<Func<T, bool>> Criteria { get; }
+    Expression<Func<T, object>> OrderBy { get; }
+    Expression<Func<T, object>> OrderByDesc { get; }
+    List<Expression<Func<T, object>>> Includes{get;}
+}
