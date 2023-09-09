3c2511ca2c31b0f4a760149d18686775ac694a0b
Wale <onafesowale@gmail.com>
2023-09-09 20:44:09 +0100

    IUnitOfWork

---
+++ src/Domain/Context/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Repositories;
+
+namespace Domain.Context;
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
+    Task<bool> Complete();
+}
