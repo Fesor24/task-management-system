972d489902c97f88b60e3812223d1452cff0fa2a
Wale <onafesowale@gmail.com>
2023-09-09 20:44:49 +0100

    IGenericRepository

---
+++ src/Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Repositories;
+public interface IGenericRepository<T> where T: class
+{
+    Task<IEnumerable<T>> GetAllAsync(ISpecification<T> spec);
+
+    Task<T> GetAsync(ISpecification<T> spec);
+
+    Task AddAsync(T entity);
+
+    bool Update(T entity);
+
+    bool Delete(T entity);
+}
