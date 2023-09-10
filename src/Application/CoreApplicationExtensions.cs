85e972ec96c4aa63062de32957a8ae96d8910558
Wale <onafesowale@gmail.com>
2023-09-10 16:04:50 +0100

    CoreApplicationExtensions

---
+++ src/Application/CoreApplicationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Application.Endpoints;
+using Application.Mediator;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+public static class CoreApplicationExtensions
+{
+    public static Type AddCoreApplication(this IServiceCollection services)
+    {
+        AddMediator(services);
+        return typeof(CoreApplicationExtensions);
+    }
+
+    public static WebApplication AddCoreApplication(this WebApplication app)
+    {
+        CommonEndpoints.AddEndpoints(app);
+
+        return app;
+    }
+
+    private static void AddMediator(IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(MediatorValidationPipelineBehavior<,>));
+    }
+}
