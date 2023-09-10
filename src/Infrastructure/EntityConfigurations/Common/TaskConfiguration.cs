397cc3fb3b231fe235361dcb8cff9a0d2fa9a535
Wale <onafesowale@gmail.com>
2023-09-10 16:01:40 +0100

    modified table name

--- src/Infrastructure/EntityConfigurations/Common/TaskConfiguration.cs
+++ src/Infrastructure/EntityConfigurations/Common/TaskConfiguration.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities.Common.Task;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using TaskEntity = Domain.Entities.Common.Task;
+using TaskEntity = Domain.Entities.Common.Task.Task;
 
 namespace Infrastructure.EntityConfigurations.Common;
 public class TaskConfiguration : IEntityTypeConfiguration<TaskEntity>
 {
     public void Configure(EntityTypeBuilder<TaskEntity> builder)
     {
-        builder.ToTable(nameof(Task), "com");
-        builder.Property(x => x.Title).IsRequired().HasColumnName("Title");
-        builder.Property(x => x.Status).HasColumnName("Status");
-        builder.Property(x => x.Priority).HasColumnName("Priority");
-        builder.Property(x => x.DueDate).HasColumnName("DueDate");
-        builder.Property(x => x.Description).IsRequired(false).HasColumnName("Description");
+        builder.ToTable("Tasks", "com");
+
+        builder.HasKey(x => x.TaskId);
+
+        builder.Property(x => x.TaskId).HasConversion(
+            task => task.Value,
+            value => new TaskId(value));
+
+        builder.Property(x => x.DueDate).HasConversion(
+            dueDate => dueDate.Value,
+            value => DueDate.Create(value)
+            );
     }
 }
