4430461c6d2cf87984d97fab6d6c9be40752dcd1
Wale <onafesowale@gmail.com>
2023-09-10 16:00:12 +0100

    IDefinition

---
+++ src/Api/Definitions/Extensions/IDefinition.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Definitions.Extensions;
+
+public interface IDefinition
+{
+    void DefineServices(IServiceCollection services, IConfiguration configuration);
+    void DefinePipelineAndRoutesConfiguration(WebApplication app,  IConfiguration configuration);
+}
