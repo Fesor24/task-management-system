d5bfef5889404c25d610a14876b7c9223fae0194
Wale <onafesowale@gmail.com>
2023-09-10 16:03:48 +0100

    get tasks functionality

---
+++ src/Application/Handlers/Common/Task/Queries/GetTasks/GetTasksRequest.cs
@@ -0,0 +1,7 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Queries.GetTasks;
+public class GetTasksRequest : IRequest<IReadOnlyList<GetTaskResponse>>
+{
+}
