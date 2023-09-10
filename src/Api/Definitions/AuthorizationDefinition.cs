b6ec218be20f584e059d7263d2ac7807fd441011
Wale <onafesowale@gmail.com>
2023-09-10 16:02:27 +0100

    authorization definition

---
+++ src/Api/Definitions/AuthorizationDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Api.Definitions.Extensions;
+
+namespace Api.Definitions;
+
+public class AuthorizationDefinition : IDefinition
+{
+    public void DefinePipelineAndRoutesConfiguration(WebApplication app, IConfiguration configuration)
+    {
+        app.UseHttpsRedirection();
+        app.UseCors("CorsPolicy");
+        //app.UseAuthorization();
+    }
+
+    public void DefineServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+            });
+        });
+    }
+}
