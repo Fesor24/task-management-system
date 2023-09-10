3423e59086c7b15418ee7713b8e73050a6edad22
Wale <onafesowale@gmail.com>
2023-09-10 15:59:10 +0100

    migration files

---
+++ src/Infrastructure/Migrations/20230910141545_init.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "com");
+
+            migrationBuilder.CreateTable(
+                name: "TaskEntity",
+                schema: "com",
+                columns: table => new
+                {
+                    TaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    DueDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskEntity", x => x.TaskId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskEntity",
+                schema: "com");
+        }
+    }
+}
---
+++ src/Infrastructure/Migrations/20230910145225_modifiedtablename.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifiedtablename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskEntity",
+                schema: "com",
+                table: "TaskEntity");
+
+            migrationBuilder.RenameTable(
+                name: "TaskEntity",
+                schema: "com",
+                newName: "Tasks",
+                newSchema: "com");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                schema: "com",
+                table: "Tasks",
+                column: "TaskId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                schema: "com",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                schema: "com",
+                newName: "TaskEntity",
+                newSchema: "com");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskEntity",
+                schema: "com",
+                table: "TaskEntity",
+                column: "TaskId");
+        }
+    }
+}
