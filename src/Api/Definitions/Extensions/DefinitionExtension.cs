821d1c9e18759d0e6109f8d5ce670a7876ed3aff
Wale <onafesowale@gmail.com>
2023-09-10 16:00:01 +0100

    definition extension

---
+++ src/Api/Definitions/Extensions/DefinitionExtension.cs
@@ -0,0 +1,42 @@
+﻿namespace Api.Definitions.Extensions;
+
+public static class DefinitionExtension
+{
+    public static void AddServicesToContainer(this IServiceCollection services, IConfiguration config, 
+        params Type[] scanMarkers)
+    {
+        var definitions = new List<IDefinition>();
+
+        foreach (var marker in scanMarkers)
+        {
+            definitions.AddRange(marker.Assembly.ExportedTypes
+                .Where(x => typeof(IDefinition).IsAssignableFrom(x) && !x.IsAbstract && !x.IsInterface)
+                .Select(Activator.CreateInstance).Cast<IDefinition>()
+                );
+        }
+
+        foreach(var definition in definitions)
+        {
+            definition.DefineServices(services, config);
+        }
+
+        services.AddSingleton(definitions as IReadOnlyCollection<IDefinition>);
+    }
+
+    public static void ConfigureHttpPipelineAndRoutes(this WebApplication app, IConfiguration config)
+    {
+        var definitions = app.Services.GetRequiredService<IReadOnlyCollection<IDefinition>>().AsEnumerable();
+
+        var swaggerDefinition = definitions.Where(d => d.GetType() == typeof(SwaggerDefinition)).SingleOrDefault();
+
+        if(swaggerDefinition is not null)
+        {
+            swaggerDefinition.DefinePipelineAndRoutesConfiguration(app, config);
+        }
+
+        foreach(var definition in definitions)
+        {
+            definition.DefinePipelineAndRoutesConfiguration(app, config);
+        }
+    }
+}
