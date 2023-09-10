732fc3ae47ba535cc4b172749cbd95b3dceb7257
Wale <onafesowale@gmail.com>
2023-09-10 16:01:55 +0100

    swagger definition

---
+++ src/Api/Definitions/SwaggerDefinition.cs
@@ -0,0 +1,30 @@
+﻿using Api.Definitions.Extensions;
+using Shared.EndpointGroupings;
+using Microsoft.OpenApi.Models;
+
+namespace Api.Definitions;
+
+public class SwaggerDefinition : IDefinition
+{
+    public void DefinePipelineAndRoutesConfiguration(WebApplication app, IConfiguration configuration)
+    {
+        app.UseSwagger();
+
+        app.UseSwaggerUI(opt =>
+        {
+            opt.SwaggerEndpoint($"/swagger/{EndpointGroupNames.MAIN}/swagger.json", EndpointGroupNames.MAIN + " endpoints");
+            opt.SwaggerEndpoint($"/swagger/{EndpointGroupNames.ACCOUNT}/swagger.json", EndpointGroupNames.ACCOUNT + " endpoints");
+        });
+    }
+
+    public void DefineServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc(EndpointGroupNames.ACCOUNT, new OpenApiInfo { Title = EndpointGroupNames.ACCOUNT + " endpoints" });
+            opt.SwaggerDoc(EndpointGroupNames.MAIN, new OpenApiInfo { Title = EndpointGroupNames.MAIN + " endpoints" });
+        });
+    }
+}
