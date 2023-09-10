90103558e68d9c90dbc3d500ab7bbdc0bbdfe3eb
Wale <onafesowale@gmail.com>
2023-09-10 16:02:14 +0100

    application layer definition

---
+++ src/Api/Definitions/ApplicationLayerDefinition.cs
@@ -0,0 +1,28 @@
+﻿using Api.Definitions.Extensions;
+using Application;
+using FluentValidation;
+using MediatR;
+using System.Reflection;
+
+namespace Api.Definitions;
+
+public class ApplicationLayerDefinition : IDefinition
+{
+    public void DefinePipelineAndRoutesConfiguration(WebApplication app, IConfiguration configuration)
+    {
+        app.AddCoreApplication();
+    }
+
+    public void DefineServices(IServiceCollection services, IConfiguration configuration)
+    {
+        IEnumerable<Assembly> domainAssemblies = new List<Type>()
+        {
+            services.AddCoreApplication()
+        }.Select(t => t.Assembly);
+
+        services
+            .AddMediatR(domainAssemblies.ToArray())
+            .AddValidatorsFromAssemblies(domainAssemblies)
+            .AddAutoMapper(domainAssemblies);
+    }
+}
