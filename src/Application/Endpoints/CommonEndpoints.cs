d4eecb1ec1d9e63f4f32434993ccac85b60159ad
Wale <onafesowale@gmail.com>
2023-09-10 16:03:21 +0100

    common endpoints

---
+++ src/Application/Endpoints/CommonEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using Application.Handlers.Common.Task.Commands.Create;
+using Application.Mediator;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+
+namespace Application.Endpoints;
+public static class CommonEndpoints
+{
+    public static void AddEndpoints(WebApplication app)
+    {
+        const string ENDPOINT = "Common";
+
+        app.MediatorPost<CreateTaskCommand, Unit>("Task", ENDPOINT);
+    }
+}
