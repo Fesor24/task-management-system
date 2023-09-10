d087037d65b49367d451a8ac9634348551a1ab35
Wale <onafesowale@gmail.com>
2023-09-10 16:03:33 +0100

    create task functionality

---
+++ src/Application/Handlers/Common/Task/Commands/Create/CreateTaskCommand.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enums;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Commands.Create;
+public record CreateTaskCommand(
+    string Title, 
+    string Description, 
+    DateTimeOffset DueDate, 
+    Status Status, 
+    Priority Priority) : IRequest<Unit>;
+
+public class CreateTaskValidator: AbstractValidator<CreateTaskCommand>
+{
+    public CreateTaskValidator()
+    {
+        RuleFor(x => x.DueDate)
+            .GreaterThanOrEqualTo(DateTimeOffset.UtcNow)
+            .WithMessage("Due Date can not be in the past");
+
+        RuleFor(x => x.Title)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Title can not be null or empty");
+
+        RuleFor(x => x.Description)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Title can not be null or empty");
+    }
+}
---
+++ src/Application/Handlers/Common/Task/Commands/Create/CreateTaskCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Context;
+using MediatR;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+using Domain.Entities.Common.Task;
+
+namespace Application.Handlers.Common.Task.Commands.Create;
+public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateTaskCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(CreateTaskCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var task = new TaskEntity(
+            new TaskId(Guid.NewGuid()),
+            request.Title,
+            request.Description,
+            DueDate.Create(request.DueDate),
+            request.Status,
+            request.Priority
+            );
+
+        await _unitOfWork.Repository<TaskEntity>().AddAsync(task);
+
+        await _unitOfWork.Complete(cancellationToken);
+
+        return Unit.Value;
+    }
+}
