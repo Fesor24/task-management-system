c393ef2d10007f65be6883f11354a22a8da193b0
Wale <onafesowale@gmail.com>
2023-09-11 11:33:37 +0100

    Create project functionality

---
+++ src/Application/Handlers/Common/Project/Commands/Create/CreateProjectCommand.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Handlers.Common.Project.Commands.Create;
+public record CreateProjectCommand(
+    string Name,
+    string Description
+    ) : IRequest<int>;
+
+public class CreateProjectValidator : AbstractValidator<CreateProjectCommand>
+{
+    public CreateProjectValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Name can not be null or empty");
+
+        RuleFor(x => x.Description)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Description can not be null or empty");
+    }
+}
---
+++ src/Application/Handlers/Common/Project/Commands/Create/CreateProjectCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Context;
+using MediatR;
+using ProjectEntity = Domain.Entities.Common.Project.Project;
+
+namespace Application.Handlers.Common.Project.Commands.Create;
+public sealed class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand, int>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateProjectCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+    {
+        var project = new ProjectEntity(
+            request.Name,
+            request.Description);
+
+        await _unitOfWork.Repository<ProjectEntity>().AddAsync(project);
+
+        await _unitOfWork.Complete(cancellationToken);
+
+        return project.ProjectId;
+    }
+}
