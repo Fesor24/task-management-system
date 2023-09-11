ec2f82f3eb4caff490934b621ce41e091a9ea037
Wale <onafesowale@gmail.com>
2023-09-11 11:38:33 +0100

    Get task by priority functionality

---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByPriority/GetTaskByPriorityRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using AutoMapper;
+using Domain.Context;
+using Infrastructure.Specifications.Task;
+using MediatR;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByPriority;
+public sealed class GetTaskByPriorityRequestHandler : IRequestHandler<GetTasksByPriorityRequest,
+    IReadOnlyList<GetTaskResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetTaskByPriorityRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<GetTaskResponse>> Handle(GetTasksByPriorityRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var taskSpec = new GetTasksByPrioritySpecification(request.Priority);
+
+        var tasks = await _unitOfWork.Repository<TaskEntity>().GetAllAsync(taskSpec);
+
+        return _mapper.Map<IReadOnlyList<GetTaskResponse>>(tasks);
+    }
+}
---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByPriority/GetTasksByPriorityRequest.cs
@@ -0,0 +1,6 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByPriority;
+public record GetTasksByPriorityRequest(Priority Priority) : IRequest<IReadOnlyList<GetTaskResponse>>;
