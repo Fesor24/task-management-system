953bebd44687d581164a79bc8fd4f56e157ae63f
Wale <onafesowale@gmail.com>
2023-09-11 20:01:15 +0100

    User context definition

---
+++ src/Api/Definitions/UserContextDefinition.cs
@@ -0,0 +1,19 @@
+﻿using Api.Definitions.Extensions;
+using Api.Helpers;
+using Domain.Services.Users;
+
+namespace Api.Definitions;
+
+public class UserContextDefinition : IDefinition
+{
+    public void DefinePipelineAndRoutesConfiguration(WebApplication app, IConfiguration configuration)
+    {
+        
+    }
+
+    public void DefineServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUserContext, UserContext>();
+    }
+}
