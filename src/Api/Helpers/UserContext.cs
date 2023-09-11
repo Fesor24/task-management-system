36b6c4f31a19ad294201e266f569cf1f5552ffd1
Wale <onafesowale@gmail.com>
2023-09-11 20:01:26 +0100

    User context

---
+++ src/Api/Helpers/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Services.Users;
+using System.Security.Claims;
+
+namespace Api.Helpers;
+
+public class UserContext : IUserContext
+{
+    private readonly ClaimsPrincipal _claims;
+
+    public UserContext(IHttpContextAccessor contextAccessor)
+    {
+        _claims = contextAccessor?.HttpContext?.User;
+    }
+
+    public int UserId { get
+        {
+            var userId = _claims.FindFirstValue(ClaimTypes.NameIdentifier);
+            return userId == null ? default(int) : int.Parse(userId);
+        }
+    }
+
+    public string Email { get
+        {
+            return _claims.FindFirstValue(ClaimTypes.Email);
+        } 
+    }
+}
