a990026d42a3d327d199e420bdea4c1d7d4aaba1
Wale <onafesowale@gmail.com>
2023-09-11 20:07:30 +0100

    registered services

--- src/Application/CoreApplicationExtensions.cs
+++ src/Application/CoreApplicationExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Endpoints;
 using Application.Mediator;
+using Application.Services.Users;
+using Domain.Services.Users;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,14 +12,21 @@ public static class CoreApplicationExtensions
     public static Type AddCoreApplication(this IServiceCollection services)
     {
         AddMediator(services);
+        AddServices(services);
         return typeof(CoreApplicationExtensions);
     }
 
     public static WebApplication AddCoreApplication(this WebApplication app)
     {
         CommonEndpoints.AddEndpoints(app);
+        AccountEndpoints.AddEndpoints(app);
 
         return app;
+    }
+
+    public static void AddServices(IServiceCollection services)
+    {
+        services.AddScoped<ISecurityService, SecurityService>();
     }
 
     private static void AddMediator(IServiceCollection services)
