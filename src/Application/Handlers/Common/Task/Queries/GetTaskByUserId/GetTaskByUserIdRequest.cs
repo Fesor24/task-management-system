f89b6b84beb40501c9cee6577e7306f41496a0bc
Wale <onafesowale@gmail.com>
2023-09-11 20:03:41 +0100

    Get task by user id functionality

---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByUserId/GetTaskByUserIdRequest.cs
@@ -0,0 +1,5 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByUserId;
+public record GetTaskByUserIdRequest : IRequest<IReadOnlyList<GetTaskResponse>>;
---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByUserId/GetTaskByUserIdRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using Domain.Context;
+using TaskEntity =  Domain.Entities.Common.Task.Task;
+using Domain.Services.Users;
+using Infrastructure.Specifications.Task;
+using MediatR;
+using AutoMapper;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByUserId;
+public class GetTaskByUserIdRequestHandler : IRequestHandler<GetTaskByUserIdRequest, 
+    IReadOnlyList<GetTaskResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserContext _userContext;
+    private readonly IMapper _mapper;
+
+    public GetTaskByUserIdRequestHandler(IUnitOfWork unitOfWork, IUserContext userContext,
+        IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _userContext = userContext;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<GetTaskResponse>> Handle(GetTaskByUserIdRequest request, CancellationToken cancellationToken)
+    {
+        var taskSpec = new GetTasksByUserIdSpecification(_userContext.UserId);
+
+        var tasks = await _unitOfWork.Repository<TaskEntity>().GetAllAsync(taskSpec);
+
+        return _mapper.Map<IReadOnlyList<GetTaskResponse>>(tasks);
+    }
+}
