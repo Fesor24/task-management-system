25a5a9d327f08be9a6f71c1333e2470ace182815
Wale <onafesowale@gmail.com>
2023-09-11 11:39:06 +0100

    Update Task functionality

---
+++ src/Application/Handlers/Common/Task/Commands/Update/UpdateTaskCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Commands.Update;
+public record UpdateTaskCommand(int TaskId,
+    string Title, 
+    string Description,
+    Status Status,
+    Priority Priority,
+    DateTime DueDate) : 
+    IRequest<Unit>;
---
+++ src/Application/Handlers/Common/Task/Commands/Update/UpdateTaskCommandHadler.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Context;
+using Infrastructure.Specifications.Task;
+using MediatR;
+using Shared.Exceptions;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+using Domain.Entities.Common.Task;
+
+namespace Application.Handlers.Common.Task.Commands.Update;
+public sealed class UpdateTaskCommandHadler : IRequestHandler<UpdateTaskCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateTaskCommandHadler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
+    {
+        var taskSpec = new GetTaskByIdSpecification(request.TaskId);
+
+        var task = await _unitOfWork.Repository<TaskEntity>().GetAsync(taskSpec);
+
+        if (task is null)
+            throw new ApiNotFoundException($"Task with Id: {request.TaskId} not found");
+
+        task.Update(new Body(request.Title, request.Description), DueDate.Create(request.DueDate), 
+            request.Status, request.Priority);
+
+        _unitOfWork.Repository<TaskEntity>().Update(task);
+
+        return Unit.Value;
+    }
+}
