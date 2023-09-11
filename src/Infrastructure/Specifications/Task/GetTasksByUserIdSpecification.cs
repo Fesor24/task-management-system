0d681df62a50f0904714162db2bd68b87f030760
Wale <onafesowale@gmail.com>
2023-09-11 11:34:37 +0100

    GetTasksById Specification

---
+++ src/Infrastructure/Specifications/Task/GetTasksByUserIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using TaskEntity = Domain.Entities.Common.Task.Task;
+
+namespace Infrastructure.Specifications.Task;
+public class GetTasksByUserIdSpecification  :BaseSpecification<TaskEntity>
+{
+    public GetTasksByUserIdSpecification()
+    {
+        AddInclude(x => x.Project);
+        SetOrderByDesc(x => x.DueDate);
+    }
+}
