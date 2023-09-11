b3649804de075f87e948b39017b8299d5fea39cf
Wale <onafesowale@gmail.com>
2023-09-11 11:38:13 +0100

    Get task by status functionailty

---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByStatus/GetTasksByStatusRequest.cs
@@ -0,0 +1,6 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByStatus;
+public record GetTasksByStatusRequest(Status Status) : IRequest<IReadOnlyList<GetTaskResponse>>;
---
+++ src/Application/Handlers/Common/Task/Queries/GetTaskByStatus/GetTasksByStatusRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using AutoMapper;
+using Domain.Context;
+using Infrastructure.Specifications.Task;
+using MediatR;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+
+namespace Application.Handlers.Common.Task.Queries.GetTaskByStatus;
+public class GetTasksByStatusRequestHandler : IRequestHandler<GetTasksByStatusRequest,
+    IReadOnlyList<GetTaskResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetTasksByStatusRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<GetTaskResponse>> Handle(GetTasksByStatusRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var spec = new GetTasksByStatusSpecification(request.Status);
+
+        var tasks = await _unitOfWork.Repository<TaskEntity>().GetAllAsync(spec);
+
+        return _mapper.Map<IReadOnlyList<GetTaskResponse>>(tasks);
+    }
+}
