677e0c2dc8b54f7ccb6a8a2c1c46f909e20313bb
Wale <onafesowale@gmail.com>
2023-09-11 20:04:38 +0100

    ISecurityService

---
+++ src/Domain/Services/Users/ISecurityService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities.Users;
+
+namespace Domain.Services.Users;
+public interface ISecurityService
+{
+    string GenerateJwts(User user);
+
+    string HashPassword(string password);
+}
