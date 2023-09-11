4ab51e82c18340810c5f106e8118bafb74c7b736
Wale <onafesowale@gmail.com>
2023-09-11 20:01:50 +0100

    Added account endpoint

---
+++ src/Application/Endpoints/AccountEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Application.Handlers.Account.Command.Login;
+using Application.Handlers.Account.Command.Register;
+using Application.Mediator;
+using Microsoft.AspNetCore.Builder;
+
+namespace Application.Endpoints;
+public static class AccountEndpoints
+{
+    public static void AddEndpoints(this WebApplication app)
+    {
+        const string ENDPOINT = "Account";
+
+        app.MediatorPost<RegisterCommand, RegisterResponse>("Register", ENDPOINT);
+        app.MediatorPost<LoginCommand, LoginResponse>("Login", ENDPOINT);
+    }
+}
+
