ff1737e4205fc696e084266dc97b48246dea24ab
Wale <onafesowale@gmail.com>
2023-09-11 20:05:28 +0100

    Added user id to task entity

--- src/Domain/Entities/Common/Task/Task.cs
+++ src/Domain/Entities/Common/Task/Task.cs
@@ -2,6 +2,7 @@
 using Domain.Enums;
 using Domain.Entities.Common.Project;
 using System.ComponentModel.DataAnnotations;
+using Domain.Entities.Users;
 
 namespace Domain.Entities.Common.Task;
 public class Task : AuditableEntity
@@ -12,13 +13,14 @@ public class Task : AuditableEntity
     }
 
     public Task(Body body, DueDate duedate, Status status, 
-        Priority priority, int? projectId)
+        Priority priority, int? projectId, int userId)
     {
         Body = body;
         DueDate = duedate.Value;
         Status = status;
         Priority = priority;
         ProjectId = projectId;
+        UserId = userId;
     }
 
     public int TaskId { get; private set; }
@@ -30,6 +32,8 @@ public class Task : AuditableEntity
     public Priority Priority { get; private set; }
     public int? ProjectId { get; private set; }
     public ProjectEntity Project { get; private set; }
+    public int? UserId { get; private set; }
+    public User User { get; private set; }
 
     public void Update(Body body,
         DueDate dueDate, Status status, Priority priority) 
