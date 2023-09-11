e5b2a44ea5c3625654ef22b192a694a423e6d366
Wale <onafesowale@gmail.com>
2023-09-11 11:35:13 +0100

    Added mappings to mapping profiles

---
+++ src/Application/MappingProfiles/CommonMappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using Application.Handlers.Common.Task.Queries.GetTask;
+using AutoMapper;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+using ProjectEntity = Domain.Entities.Common.Project.Project;
+using Application.Handlers.Common.Project.Queries.GetProject;
+
+namespace Application.MappingProfiles;
+public class CommonMappingProfiles : Profile
+{
+    public CommonMappingProfiles()
+    {
+        CreateMap<TaskEntity, GetTaskResponse>()
+            .ForMember(x => x.Title, o => o.MapFrom(t => t.Body.Title))
+            .ForMember(x => x.Description, o => o.MapFrom(t => t.Body.Description))
+            .ForMember(x => x.ProjectName, o => o.MapFrom(t => t.Project.Name));
+
+        CreateMap<ProjectEntity, GetProjectResponse>();
+    }
+}
