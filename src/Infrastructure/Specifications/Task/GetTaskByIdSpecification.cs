4a0f9cb4623811ca015e02900e42912af1314b1c
Wale <onafesowale@gmail.com>
2023-09-11 11:32:58 +0100

    modified id prop to int

---
+++ src/Infrastructure/Specifications/Task/GetTaskByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Common.Task;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+
+namespace Infrastructure.Specifications.Task;
+public class GetTaskByIdSpecification : BaseSpecification<TaskEntity>
+{
+    public GetTaskByIdSpecification(int taskId): base(x => x.TaskId == taskId)
+    {
+        AddInclude(x => x.Project);
+    }
+}
