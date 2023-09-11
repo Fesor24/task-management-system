602ca50515fd6464c9076734302a5dc4864d1601
Wale <onafesowale@gmail.com>
2023-09-11 11:33:16 +0100

    Added endpoints

--- src/Application/Endpoints/CommonEndpoints.cs
+++ src/Application/Endpoints/CommonEndpoints.cs
@@ -1,4 +1,13 @@
-﻿using Application.Handlers.Common.Task.Commands.Create;
+﻿using Application.Handlers.Common.Project.Commands.Create;
+using Application.Handlers.Common.Project.Queries.GetProject;
+using Application.Handlers.Common.Project.Queries.GetProjects;
+using Application.Handlers.Common.Task.Commands.AssignProject;
+using Application.Handlers.Common.Task.Commands.Create;
+using Application.Handlers.Common.Task.Queries.GetTask;
+using Application.Handlers.Common.Task.Queries.GetTaskByPriority;
+using Application.Handlers.Common.Task.Queries.GetTaskByStatus;
+using Application.Handlers.Common.Task.Queries.GetTasks;
+using Application.Handlers.Common.Task.Queries.GetTasksForCurrentWeek;
 using Application.Mediator;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +19,16 @@ public static class CommonEndpoints
     {
         const string ENDPOINT = "Common";
 
-        app.MediatorPost<CreateTaskCommand, Unit>("Task", ENDPOINT);
+        app.MediatorPost<CreateTaskCommand, int>("Task", ENDPOINT);
+        app.MediatorGet<GetTaskByIdRequest, GetTaskResponse>("Task", ENDPOINT);
+        app.MediatorGet<GetTasksRequest, IReadOnlyList<GetTaskResponse>>("Tasks", ENDPOINT);
+        app.MediatorGet<GetTasksForCurrentWeekRequest, IReadOnlyList<GetTaskResponse>>("Tasks/CurrentWeek", ENDPOINT);
+        app.MediatorGet<GetTasksByPriorityRequest, IReadOnlyList<GetTaskResponse>>("Tasks/Priority", ENDPOINT);
+        app.MediatorGet<GetTasksByStatusRequest, IReadOnlyList<GetTaskResponse>>("Tasks/Status", ENDPOINT);
+        app.MediatorPut<AssignProjectCommand, Unit>("Task/AssignProject", ENDPOINT);
+
+        app.MediatorPost<CreateProjectCommand, int>("Project", ENDPOINT);
+        app.MediatorGet<GetProjectByIdRequest, GetProjectResponse>("Project", ENDPOINT);
+        app.MediatorGet<GetProjectsRequest, IReadOnlyList<GetProjectResponse>>("Projects", ENDPOINT);
     }
 }
