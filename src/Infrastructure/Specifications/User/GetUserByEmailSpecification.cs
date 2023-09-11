66b14a952bcbdcfae2af118b70e9f035d1e0a816
Wale <onafesowale@gmail.com>
2023-09-11 20:07:59 +0100

    get user by email specification

---
+++ src/Infrastructure/Specifications/User/GetUserByEmailSpecification.cs
@@ -0,0 +1,10 @@
+﻿using UserEntity = Domain.Entities.Users.User;
+
+namespace Infrastructure.Specifications.User;
+public class GetUserByEmailSpecification : BaseSpecification<UserEntity>
+{
+    public GetUserByEmailSpecification(string email) : base(x => x.UserInfo.Email ==  email)
+    {
+        
+    }
+}
