4d08cf28dafc8a4f40b259dc9757187064a1ba41
Wale <onafesowale@gmail.com>
2023-09-11 20:08:08 +0100

    Jwt bearer settings

---
+++ src/Shared/AppSettings/JwtBearerSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.AppSettings;
+public class JwtBearerSettings
+{
+    public const string ConfigurationName = "JwtBearerSettings";
+
+    public string Scheme { get; set; }
+    public string Issuer { get; set; }
+    public string Key { get; set; }
+    public string AuthorizationHeaderName { get; set; }
+    public int SessionTimeout { get; set; }
+}
