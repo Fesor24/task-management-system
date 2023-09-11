df5e04c3e473cb438557e2f43b7ca4c28b661a26
Wale <onafesowale@gmail.com>
2023-09-11 11:41:05 +0100

    Changed id type to int

---
+++ src/Infrastructure/Specifications/Project/GetProjectByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Common.Project;
+using ProjectEntity = Domain.Entities.Common.Project.Project;
+
+namespace Infrastructure.Specifications.Project;
+public class GetProjectByIdSpecification : BaseSpecification<ProjectEntity>
+{
+    public GetProjectByIdSpecification(int projectId): base(x => x.ProjectId == projectId)
+    {
+        AddInclude(x => x.Tasks);
+    }
+}
