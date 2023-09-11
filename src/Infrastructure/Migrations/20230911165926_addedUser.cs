675c0bf3ab8480f52acc9e9ad1ad2dd8dc78bf91
Wale <onafesowale@gmail.com>
2023-09-11 20:04:05 +0100

    migration files

---
+++ src/Infrastructure/Migrations/20230911165926_addedUser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "usr");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                schema: "com",
+                table: "Tasks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "usr",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserInfo_Name = table.Column<string>(type: "text", nullable: false),
+                    UserInfo_Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                schema: "com",
+                table: "Tasks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                schema: "com",
+                table: "Tasks",
+                column: "UserId",
+                principalSchema: "usr",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                schema: "com",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "usr");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserId",
+                schema: "com",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                schema: "com",
+                table: "Tasks");
+        }
+    }
+}
