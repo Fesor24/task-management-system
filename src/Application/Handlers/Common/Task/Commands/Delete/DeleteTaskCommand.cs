bf16b2c9757e694dfe427024fc7f1ac9e812a5ce
Wale <onafesowale@gmail.com>
2023-09-11 20:03:25 +0100

    delete task functionality

---
+++ src/Application/Handlers/Common/Task/Commands/Delete/DeleteTaskCommand.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+
+namespace Application.Handlers.Common.Task.Commands.Delete;
+public record DeleteTaskCommand(int TaskId) : IRequest<Unit>;
---
+++ src/Application/Handlers/Common/Task/Commands/Delete/DeleteTaskCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Context;
+using Infrastructure.Specifications.Task;
+using MediatR;
+using Shared.Exceptions;
+using TaskEntity = Domain.Entities.Common.Task.Task;
+
+namespace Application.Handlers.Common.Task.Commands.Delete;
+public sealed class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteTaskCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+    {
+        var taskSpec = new GetTaskByIdSpecification(request.TaskId);
+
+        var task = await _unitOfWork.Repository<TaskEntity>().GetAsync(taskSpec);
+
+        if (task is null)
+            throw new ApiNotFoundException($"Task with TaskId: {request.TaskId} not found");
+
+        _unitOfWork.Repository<TaskEntity>().Delete(task);
+
+        await _unitOfWork.Complete(cancellationToken);
+
+        return Unit.Value;
+    }
+}
