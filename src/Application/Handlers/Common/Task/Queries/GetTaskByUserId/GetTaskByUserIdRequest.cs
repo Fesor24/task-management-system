d08eceba5783cf75788df95b5c89b563f70130e9
Wale <onafesowale@gmail.com>
2023-09-13 01:52:37 +0100

    removed files

--- src/Application/Handlers/Common/Task/Queries/GetTaskByUserId/GetTaskByUserIdRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Handlers.Common.Task.Queries.GetTask;
-using MediatR;
-
-namespace Application.Handlers.Common.Task.Queries.GetTaskByUserId;
-public record GetTaskByUserIdRequest : IRequest<IReadOnlyList<GetTaskResponse>>;
