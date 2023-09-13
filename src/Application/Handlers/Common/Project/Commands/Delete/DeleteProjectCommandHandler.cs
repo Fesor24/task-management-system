349296fb797ba118fa815d5076ae9b0d5b113f09
Wale <onafesowale@gmail.com>
2023-09-13 01:56:41 +0100

    injected user context

--- src/Application/Handlers/Common/Project/Commands/Delete/DeleteProjectCommandHandler.cs
+++ src/Application/Handlers/Common/Project/Commands/Delete/DeleteProjectCommandHandler.cs
@@ -3,20 +3,23 @@ using ProjectEntity =  Domain.Entities.Common.Project.Project;
 using Infrastructure.Specifications.Project;
 using MediatR;
 using Shared.Exceptions;
+using Domain.Services.Users;
 
 namespace Application.Handlers.Common.Project.Commands.Delete;
 public sealed class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand, Unit>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserContext _userContext;
 
-    public DeleteProjectCommandHandler(IUnitOfWork unitOfWork)
+    public DeleteProjectCommandHandler(IUnitOfWork unitOfWork, IUserContext userContext)
     {
         _unitOfWork = unitOfWork;
+        _userContext = userContext;
     }
 
     public async Task<Unit> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
     {
-        var projectSpec = new GetProjectByIdSpecification(request.ProjectId);
+        var projectSpec = new GetProjectByIdSpecification(request.ProjectId, _userContext.UserId);
 
         var project = await _unitOfWork.Repository<ProjectEntity>().GetAsync(projectSpec);
 
