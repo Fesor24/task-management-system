6f0d7e193767d217ea6ad8463ee4773b860a874f
Wale <onafesowale@gmail.com>
2023-09-13 01:47:52 +0100

    notifications spec

---
+++ src/Infrastructure/Specifications/Notification/GetNotificationByTaskIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using NotificationEntity =  Domain.Entities.Common.Notification.Notification;
+
+namespace Infrastructure.Specifications.Notification;
+public class GetNotificationByTaskIdSpecification : BaseSpecification<NotificationEntity>
+{
+    public GetNotificationByTaskIdSpecification(int taskId) : base(x => x.TaskId == taskId && 
+        x.NotificationType == Domain.Enums.NotificationType.TaskReminder)
+    {
+        
+    }
+}
---
+++ src/Infrastructure/Specifications/Notification/GetNotificationsByUserIdSpecification.cs
@@ -0,0 +1,10 @@
+﻿using NotificationEntity = Domain.Entities.Common.Notification.Notification;
+
+namespace Infrastructure.Specifications.Notification;
+public class GetNotificationsByUserIdSpecification : BaseSpecification<NotificationEntity>
+{
+    public GetNotificationsByUserIdSpecification(int userId) : base(x => x.UserId == userId)
+    {
+        
+    }
+}
