54c1b84b1990e1a4502c696f550daac4da3e35dc
Wale <onafesowale@gmail.com>
2023-09-13 01:54:49 +0100

    notification entity

---
+++ src/Domain/Entities/Common/Notification/Notification.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities.Common.Notification;
+public class Notification : AuditableEntity
+{
+    public Notification()
+    {
+        
+    }
+
+    public Notification(int? userId, string message, NotificationType notificationType, int taskId)
+    {
+        UserId = userId;
+        Message = message;
+        NotificationType = notificationType;
+        NotificationStatus = NotificationStatus.Unread;
+        TaskId = taskId;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public int NotificationId { get; private set; }
+    public string Message { get; private set; }
+    public int? UserId { get; private set; }
+    public int TaskId { get; private set; }
+    public NotificationStatus NotificationStatus { get; private set; }
+    public NotificationType NotificationType { get; private set; }
+}
