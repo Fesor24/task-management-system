399684ed7d09b413b3ba6fcd3ec49974df358c26
Wale <onafesowale@gmail.com>
2023-09-13 01:51:56 +0100

    worker service

---
+++ src/Application/Services/Worker/BackgroundJobs.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Services.Notification;
+using Hangfire;
+
+namespace Application.Services.Worker;
+public class BackgroundJobs
+{
+    private readonly IRecurringJobManager _recurringJobManager;
+
+    public BackgroundJobs(IRecurringJobManager recurringJobManager)
+    {
+        _recurringJobManager = recurringJobManager;
+    }
+
+    // Task will be executed every 10 minutes
+    // Reminder notifications will only be sent once in respect of a task
+    public void DueTaskUserReminderNotification()
+    {
+        _recurringJobManager.AddOrUpdate<INotificationService>(JobIdentifier.TASKREMINDER, (method) => method.SetTaskReminder(), 
+            "*/10 * * * *");
+    }
+}
---
+++ src/Application/Services/Worker/JobIdentifier.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Services.Worker;
+public class JobIdentifier
+{
+    public const string TASKREMINDER = "7DACD29F-8417-42E9-BEE8-4F83F1FE8810";
+}
