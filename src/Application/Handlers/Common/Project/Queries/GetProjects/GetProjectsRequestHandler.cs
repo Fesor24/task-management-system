0211d6828c7531a4e015994859b6aa09e1127c30
Wale <onafesowale@gmail.com>
2023-09-13 23:50:07 +0100

    inject user context

--- src/Application/Handlers/Common/Project/Queries/GetProjects/GetProjectsRequestHandler.cs
+++ src/Application/Handlers/Common/Project/Queries/GetProjects/GetProjectsRequestHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Handlers.Common.Project.Queries.GetProject;
 using AutoMapper;
 using Domain.Context;
+using Domain.Services.Users;
 using Infrastructure.Specifications.Project;
 using MediatR;
 using ProjectEntity = Domain.Entities.Common.Project.Project;
@@ -11,16 +12,18 @@ public sealed class GetProjectsRequestHandler : IRequestHandler<GetProjectsReque
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUserContext _userContext;
 
-    public GetProjectsRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetProjectsRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, IUserContext userContext)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _userContext = userContext;
     }
 
     public async Task<IReadOnlyList<GetProjectResponse>> Handle(GetProjectsRequest request, CancellationToken cancellationToken)
     {
-        var projectSpec = new GetProjectsSpecification();
+        var projectSpec = new GetProjectsSpecification(_userContext.UserId);
 
         var projects = await _unitOfWork.Repository<ProjectEntity>().GetAllAsync(projectSpec);
 
