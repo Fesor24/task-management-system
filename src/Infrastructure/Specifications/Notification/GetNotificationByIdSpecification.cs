011ddac0def03dc0150359c811a58282e9bd567f
Wale <onafesowale@gmail.com>
2023-09-13 23:50:26 +0100

    get notification by id spec

---
+++ src/Infrastructure/Specifications/Notification/GetNotificationByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using NotificationEntity = Domain.Entities.Common.Notification.Notification;
+
+namespace Infrastructure.Specifications.Notification;
+public class GetNotificationByIdSpecification : BaseSpecification<NotificationEntity>
+{
+    public GetNotificationByIdSpecification(int notificationId, int userId) : base(x => x.NotificationId == notificationId &&
+        x.UserId == userId)
+    {
+        
+    }
+}
