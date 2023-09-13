26115c9f9f8b27289be52d790a553113cab2ca6a
Wale <onafesowale@gmail.com>
2023-09-13 01:56:55 +0100

    user context

--- src/Application/Handlers/Common/Project/Queries/GetProjectById/GetProjectByIdRequestHandler.cs
+++ src/Application/Handlers/Common/Project/Queries/GetProjectById/GetProjectByIdRequestHandler.cs
@@ -4,22 +4,26 @@ using ProjectEntity =  Domain.Entities.Common.Project.Project;
 using Infrastructure.Specifications.Project;
 using MediatR;
 using Shared.Exceptions;
+using Domain.Services.Users;
 
 namespace Application.Handlers.Common.Project.Queries.GetProject;
 public sealed class GetProjectByIdRequestHandler : IRequestHandler<GetProjectByIdRequest, GetProjectResponse>
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUserContext _userContext;
 
-    public GetProjectByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetProjectByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper,
+        IUserContext userContext)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _userContext = userContext;
     }
 
     public async Task<GetProjectResponse> Handle(GetProjectByIdRequest request, CancellationToken cancellationToken)
     {
-        var projectSpec = new GetProjectByIdSpecification(request.ProjectId);
+        var projectSpec = new GetProjectByIdSpecification(request.ProjectId, _userContext.UserId);
 
         var project = await _unitOfWork.Repository<ProjectEntity>().GetAsync(projectSpec);
 
