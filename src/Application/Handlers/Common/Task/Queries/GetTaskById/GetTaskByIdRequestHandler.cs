4dae90e698e8ae3a26a182e2705bf65821f0708a
Wale <onafesowale@gmail.com>
2023-09-13 01:49:56 +0100

    modifications to file

--- src/Application/Handlers/Common/Task/Queries/GetTaskById/GetTaskByIdRequestHandler.cs
+++ src/Application/Handlers/Common/Task/Queries/GetTaskById/GetTaskByIdRequestHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Domain.Context;
+using Domain.Services.Users;
 using Infrastructure.Specifications.Task;
 using MediatR;
 using Shared.Exceptions;
@@ -10,16 +11,18 @@ public sealed class GetTaskByIdRequestHandler : IRequestHandler<GetTaskByIdReque
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUserContext _userContext;
 
-    public GetTaskByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetTaskByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, IUserContext userContext)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _userContext = userContext;
     }
 
     public async Task<GetTaskResponse> Handle(GetTaskByIdRequest request, CancellationToken cancellationToken)
     {
-        var taskSpec = new GetTaskByIdSpecification(request.TaskId);
+        var taskSpec = new GetTaskByIdSpecification(request.TaskId, _userContext.UserId);
 
         var task = await _unitOfWork.Repository<TaskEntity>().GetAsync(taskSpec);
 
--- src/Application/Handlers/Common/Task/Queries/GetTaskById/GetTaskResponse.cs
+++ src/Application/Handlers/Common/Task/Queries/GetTaskById/GetTaskResponse.cs
@@ -7,7 +7,7 @@ public class GetTaskResponse
 
     public string Title { get; set; }
     public string Description { get; set; }
-    public Status Status { get; set; }
+    public Domain.Enums.TaskStatus Status { get; set; }
     public Priority Priority { get; set; }
     public string ProjectName { get; set; }
 
