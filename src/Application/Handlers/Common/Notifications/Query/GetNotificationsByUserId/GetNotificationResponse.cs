5e9716e1ecec2620c6507708d7f12b65bddeea92
Wale <onafesowale@gmail.com>
2023-09-14 11:56:04 +0100

    Modified prop

--- src/Application/Handlers/Common/Notifications/Query/GetNotificationsByUserId/GetNotificationResponse.cs
+++ src/Application/Handlers/Common/Notifications/Query/GetNotificationsByUserId/GetNotificationResponse.cs
@@ -1,6 +1,8 @@
 ﻿namespace Application.Handlers.Common.Notifications.Query.GetNotificationsByUserId;
 public class GetNotificationResponse
 {
+    public int NotificationId { get; set; }
+    public string NotificationStatus { get; set; }
     public string Message { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
 }
--- src/Application/MappingProfiles/CommonMappingProfiles.cs
+++ src/Application/MappingProfiles/CommonMappingProfiles.cs
@@ -14,10 +14,13 @@ public class CommonMappingProfiles : Profile
         CreateMap<TaskEntity, GetTaskResponse>()
             .ForMember(x => x.Title, o => o.MapFrom(t => t.Body.Title))
             .ForMember(x => x.Description, o => o.MapFrom(t => t.Body.Description))
-            .ForMember(x => x.ProjectName, o => o.MapFrom(t => t.Project.Name));
+            .ForMember(x => x.ProjectName, o => o.MapFrom(t => t.Project.Name))
+            .ForMember(x => x.Status, o => o.MapFrom(t => t.Status.ToString()))
+            .ForMember(x => x.Priority, o => o.MapFrom(t => t.Priority.ToString()));
 
         CreateMap<ProjectEntity, GetProjectResponse>();
 
-        CreateMap<Notification, GetNotificationResponse>();
+        CreateMap<Notification, GetNotificationResponse>()
+            .ForMember(x => x.NotificationStatus, o => o.MapFrom(t => t.NotificationStatus.ToString()));
     }
 }
